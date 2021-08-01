cbe809a597dc749c37bc7d7ae4b0ba8c4ab7a1d5
Andrew Pirkl <andrew.pirkl@jamf.com>
2021-08-01 09:32:32 -0500

    Initial Commit

---
+++ Shared/OscenApp.swift
@@ -0,0 +1,17 @@
+//
+//  OscenApp.swift
+//  Shared
+//
+//  Created by Andrew Pirkl on 8/1/21.
+//
+
+import SwiftUI
+
+@main
+struct OscenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
